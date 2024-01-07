0fccc64af74d4bd87abeb436f79ebd02629d3c28
pramod-sj <pramodsinghjantwal@gmail.com>
2024-01-07 11:47:42 +0530

    Initial Commit

---
+++ SwiftUI-Projects/SwiftUI_ProjectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ProjectsApp.swift
+//  SwiftUI-Projects
+//
+//  Created by Pramod Singh on 07/01/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ProjectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
